3e416f35607f2753058d882767406337c48db08c
RgmceTech2024 <rgmcetphotos@gmail.com>
2025-02-09 16:38:10 +0530

    Update Tech.jsx

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,17 +1,21 @@
-import React from "react";
-
+import React, { useMemo } from "react";
 import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
 
 const Tech = () => {
+  const renderedTech = useMemo(() => 
+    technologies?.map((technology) => (
+      <div className='w-28 h-28' key={technology.name}>
+        <BallCanvas icon={technology.icon} />
+      </div>
+    )), 
+    [technologies]
+  );
+
   return (
     <div className='flex flex-row flex-wrap justify-center gap-10'>
-      {technologies.map((technology) => (
-        <div className='w-28 h-28' key={technology.name}>
-          <BallCanvas icon={technology.icon} />
-        </div>
-      ))}
+      {renderedTech}
     </div>
   );
 };
